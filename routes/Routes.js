f5e0ae3438c5ff3b1e29f9b264ac5c26ecca167b
“zdenham” <zdenham@usc.edu>
2016-10-08 17:28:36 -0700

    changed to different SEO package

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,51 +1,35 @@
 if(Meteor.isClient){
-
-	Router.plugin('seo', {
-	  defaults: { 
-	  	title: "Visualify",
-	  	og: {
-	  		url: "https://beta.visualify.xyz",
-	  		title: "Visualify",
-	  		description: "my top artists and tracks",
-	  		image : "https://i.scdn.co/image/214b1108d830f4426cb182cf742a409d72317cf6"
-	  	}
-	  }
-	});
-
-	Router.route('Share/:_id', {
-			waitOn: function () {
-				return Meteor.subscribe('shares', this.params._id);
-			},
-      data: function() {
-      	var _id = this.params._id;
-      	Meteor.subscribe('shares', _id);
-      	b = Shares.find( { _id : _id } ).fetch();
-        return b[0];
-      },
-      seo: {
-        title : function () {
-        	return "Visualify"; 
-        },
-        og : {
-        	url : function () {
-        		return "https://beta.visualify.xyz/share/" + this.params._id;
-        	},
-        	title : function () {
-				return "Who Is Your Binge-Artist?";
-			},
-			image : function () {
-				return this.data().data.bgArtist.image;
-			},
-			description : function () {
-				return "My top artist this month was " + this.data().data.topShortTracks[0].bandName + 
-				". Click to see the rest of " + 
-				this.data().data.displayName.split(" ")[0] + "\'s top music and find out YOUR top artists and songs on Visualify";
-			}
-        }
-      }, action: function () {
-      	this.render("Share");
-      }
-    });
-
-
+	Router.map(function() {
+		  return this.route('Share', {
+		    path: '/share/:_id',
+		    waitOn: function() {
+		      return Meteor.subscribe('shares', this.params._id);
+		    },
+		    data: function() {
+		      	var _id = this.params._id;
+		      	Meteor.subscribe('shares', _id);
+		      	b = Shares.find( { _id : _id } ).fetch();
+		        return b[0];
+		      },
+		    onAfterAction: function() {
+		      var data;
+		      // The SEO object is only available on the client.
+		      // Return if you define your routes on the server, too.
+		      if (!Meteor.isClient || !this.data()) {
+		        return;
+		      }
+		      console.log('before this');
+		      data = this.data().data;
+		      console.log('after this');
+		      SEO.set({
+		        title : "Visualify",
+		        og : {
+		        	title : "Who Is Your Binge-Artist?",
+					image : data.bgArtist.image,
+					description : "My top artist this month was " + data.topShortTracks[0].bandName + ". Click to see the rest of "  + data.displayName.split(" ")[0] + "\'s top music and find out YOUR top artists and songs on Visualify"
+		        }
+		      });
+		    }
+		  });
+		});
 }
