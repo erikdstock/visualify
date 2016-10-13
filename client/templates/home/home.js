d3ed400b99b86ff21433e48958a42e1e14ceb9c9
“zdenham” <zdenham@usc.edu>
2016-10-12 17:18:28 -0700

    fixed small scraping bug and facebook in browser login

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -282,7 +282,7 @@ Template.Home.onCreated(function () {
 		for(var i = 0; i < 5; i++){
 			//artists
 			relevantData.topShortArtists.push( { name : template.topShortArtists.curValue[i].name, image : template.topShortArtists.curValue[i].images[0].url, link : template.topShortArtists.curValue[i].external_urls.spotify } );
-			relevantData.topMediumArtists.push( { name : template.topMediumArtists.curValue[i].name, image : template.topMediumArtists.curValue[i].images[0].url, link : template.topLongArtists.curValue[i].external_urls.spotify} );
+			relevantData.topMediumArtists.push( { name : template.topMediumArtists.curValue[i].name, image : template.topMediumArtists.curValue[i].images[0].url, link : template.topMediumArtists.curValue[i].external_urls.spotify} );
 			relevantData.topLongArtists.push( { name : template.topLongArtists.curValue[i].name, image : template.topLongArtists.curValue[i].images[0].url, link : template.topLongArtists.curValue[i].external_urls.spotify});
 
 			relevantData.topShortTracks.push({ name : template.topShortTracks.curValue[i].name, image : template.topShortTracks.curValue[i].album.images[0].url, link : template.topShortTracks.curValue[i].external_urls.spotify, bandName : template.topShortTracks.curValue[i].artists[0].name});
--- server/package.js
+++ server/package.js
@@ -9,8 +9,9 @@ ServiceConfiguration.configurations.update(
   { "service": "spotify" },
   {
     $set: {
-      "clientId": Meteor.settings.spotifyid,
-      "secret": Meteor.settings.spotifysecret
+      clientId: Meteor.settings.spotifyid,
+      secret: Meteor.settings.spotifysecret,
+      loginStyle: "redirect"
     }
   },
   { upsert: true }
